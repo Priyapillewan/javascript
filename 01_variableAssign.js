cf2a6e7c3785e87686694c51e744710d7ba17760
Priyapillewan <priyanka.pillewan@gmail.com>
2023-12-26 22:35:41 +0530

    variable assign
    variable assign

---
+++ 01_variableAssign.js
@@ -0,0 +1,33 @@
+ var myFullName;
+ console.log(myFullName);
+ var myFullName="Priyanka Pillewan";
+ console.log("First & Last Name:", myFullName );
+
+ myFullName="Priyanka Ramesh Pillewan";
+ console.log("Full Name:", myFullName);
+ console.log("----------------------------------------------------------------------------------");
+
+ var myLove="I love only JS";
+ console.log(myLove);
+ console.log("----------------------------------------------------------------------------------");
+ var firstName="Priyanka";
+ console.log("First Name:", firstName );
+
+ var lastName="Pillewan";
+ console.log("Last Name:", lastName);
+
+ var age="30";
+ console.log("Age:", age);
+
+ var address="Nagpur";
+ console.log("Address:", address);
+
+ var pinCode="441202";
+ console.log("Pin Code:", pinCode);
+
+ var gender="Female";
+ console.log("Gender:", gender);
+
+ var isMarried="Yes";
+ console.log("Is Married?",isMarried );
+ console.log("----------------------------------------------------------------------------------");
---
+++ helloWord.js
@@ -0,0 +1,29 @@
+console.log("Hello JS I am Priyanka");
+// variable declaration
+var num;
+console.log(num);
+num="10";
+console.log(num);
+console.log("Priyanka")
+
+// Variable Declaration
+var myName="priyanka";
+console.log(myName);
+console.log("First Name:", myName);
+myName="Priyanka Pillewan"; 
+console.log("Full Name:", myName);
+
+// class code
+console.log("Hello JS , I am Gajanan");
+
+var myName; // Varibale Declartion
+console.log(myName); // undefined
+
+myName = "Gajanan"; // Variable Intialization
+console.log("First Name: ",myName);
+
+var age = "32"; // Varibale declaration and Initialization
+console.log("Age:", age);
+
+myName = "Gajanan Kharat"; // Varibale update
+console.log("Full name: ", myName);
