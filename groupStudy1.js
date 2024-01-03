463a5fed4e2c2c7d7208dbf1a68c4c29ecd4a66b
Priyapillewan <priyanka.pillewan@gmail.com>
2024-01-03 14:36:51 +0530

    update
    update

---
+++ groupStudy1.js
@@ -0,0 +1,26 @@
+console.log("=========================Step1====================================");
+
+function areaOfRectangle(length, width) {
+    result = length*width;
+    console.log(`Area of rectangle = ${result}`);
+
+    
+}
+areaOfRectangle(5, 10);
+
+console.log("=========================Step2====================================");
+
+function replaceString() {
+   var arg1 = "Good Evening";
+   var result = arg1.replace("Evening", "Night");
+   console.log(`Given string is: ${arg1}`);
+   console.log(`After replace string is: ${result}`);
+
+   
+}
+
+replaceString();
+
+
+
+
